7aa486fd77df4052ee213f4c7ea3c23b52805dd1
Sherry Fu <shuang.fu@sas.com>
2023-03-22 04:20:10 +0000

    module1

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -1,3 +1,17 @@
 import sys
 import importlib
 from pathlib import Path
+
+def load_module(directory, name):
+    sys[0] = sys.path.insert(directory)
+    importlib.import_module(name)
+    sys.path.pop(sys[0])
+    
+def load_directory(directory):
+    for path in Path.rglob(".py"):
+        load_module(directory.as_posix(),path.name)
+        
+def load_bundled():
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
+    
