c18594df3720b9026e3c7bd38cfdce2077d101f9
Sherry Fu <shuang.fu@sas.com>
2023-03-22 06:29:59 +0000

    typo

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,7 +4,7 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p:  not isinstance(p, parsers.ResourceParser)
-    for path in source.rglog("*"):
+    for path in source.rglob("*"):
         for parser in site_parsers(list(filter(valid,site_parsers))):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
