06bc596baa7b518a92064f6de009bbdfa9b40dae
Sherry Fu <shuang.fu@sas.com>
2023-03-22 06:36:19 +0000

    t

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -14,5 +14,5 @@ def generate_menu(html,ext):
     template = '<li><a href="{}{}">{}</a></li>'
     lambda name, ext:template.format(name,ext)
     menu_item = lambda name, ext: template.format(name, ext, name.title())
-    menu = "\n".join({menu_item(path.stem,ext) for path in files})
+    menu = "\n".join([menu_item(path.stem,ext) for path in files])
     return "<ul>\n{}</ul>\n{}".format(menu,html)
