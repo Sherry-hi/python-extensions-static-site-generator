4dccc208d128dca8fb98d25dc0a3d157226ea218
Sherry Fu <shuang.fu@sas.com>
2023-03-22 06:32:24 +0000

    t

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -5,7 +5,7 @@ files = []
 def collect_files(source, site_parsers):
     valid = lambda p:  not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
-        for parser in site_parsers(list(filter(valid,site_parsers))):
+        for parser in site_parsers(list(filter(valid, site_parsers))):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
             
