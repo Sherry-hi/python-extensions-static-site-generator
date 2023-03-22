c2378cff1e28b22141ae69a229ddc5022d3bd4e9
Sherry Fu <shuang.fu@sas.com>
2023-03-22 07:06:06 +0000

    typo

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -17,9 +17,9 @@ def written():
 @hooks.register("stats")
 def stats():
     final_time = time.time() - start_time
-    avarage = final_time / total_written if total_written else 0
+    average = final_time / total_written if total_written else 0
     report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
-    print(report.format(total_written,final_time,avarage))
+    print(report.format(total_written,final_time,average))
     
 
     
