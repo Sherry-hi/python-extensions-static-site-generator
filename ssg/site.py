c4481140083837b62450d5197ff3491a92145a57
Sherry Fu <shuang.fu@sas.com>
2023-03-22 07:09:32 +0000

    a

--- ssg/site.py
+++ ssg/site.py
@@ -29,6 +29,7 @@ class Site:
 
     def build(self):
         extensions.load_bundled()
+        hooks.event("collect_files", self.source, self.parsers)
         hooks.event("start_build")
         
         self.dest.mkdir(parents=True, exist_ok=True)
